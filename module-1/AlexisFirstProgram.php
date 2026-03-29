50e1c5d3d62775b747634442a6e3dcd65ac6480c
alexispaige247 <alexispaige247@gmail.com>
2026-03-29 07:01:52 -0700

    first php program and word doc

---
+++ module-1/AlexisFirstProgram.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First PHP Program</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+    <?php
+    // Snippet displays a simple message
+        echo "This is my first PHP program!";
+    ?>
+    
+    <?php
+    // Snippet performs simple addition
+    $number1 = 10;
+    $number2 = 20;
+    $sum = $number1 + $number2;
+    echo "The sum of $number1 and $number2 is: $sum";
+    ?>
+</body>
+</html>
